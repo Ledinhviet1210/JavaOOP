413ff32271f12f2595612d8647eeb124ceae4613
Ledinhviet1210 <ledinhviet12102004@gmail.com>
2024-03-07 11:52:56 +0700

    Slide35Chuong2

---
+++ BaiTapThucHanh/src/Slide35Chuong2.java
@@ -0,0 +1,19 @@
+
+    import java.util.Scanner;
+
+public class Slide35Chuong2 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap a :");
+        int a = sc.nextInt();
+        System.out.print("Nhap b :");
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println("Min là : " +b);
+        }else{
+            System.out.println("Min là :" +a);
+        }
+}
+}
+
+
