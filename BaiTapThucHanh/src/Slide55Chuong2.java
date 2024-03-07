7e0b9cdafec04e332df76b361d7539b37f191b90
Ledinhviet1210 <ledinhviet12102004@gmail.com>
2024-03-07 11:58:50 +0700

    Slide55chuong2

---
+++ BaiTapThucHanh/src/Slide55Chuong2.java
@@ -0,0 +1,10 @@
+public class Slide55Chuong2 {
+    public static void main(String[] args) {
+        for(int i=0;i<=20;i++){
+            if(i%5==0){
+                System.out.print("Cac so chia het cho 5 la " +i);
+            }
+        }
+    }
+}
+
