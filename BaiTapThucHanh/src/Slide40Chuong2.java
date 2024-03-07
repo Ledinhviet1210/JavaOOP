fba576f8b45370a74ccff10dff703ce7ec3e082b
Ledinhviet1210 <ledinhviet12102004@gmail.com>
2024-03-07 11:54:52 +0700

    Slide40Chuong2

---
+++ BaiTapThucHanh/src/Slide40Chuong2.java
@@ -0,0 +1,38 @@
+
+    import java.util.Scanner;
+
+public class Slide40Chuong2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ngay :");
+        int ngay = sc.nextInt();
+        switch (ngay) {
+            case 1:
+                System.out.println("Thứ 2");
+                break;
+            case 2:
+                System.out.println("Thứ ba");
+                break;
+            case 3:
+                System.out.println("Thứ tư");
+                break;
+            case 4:
+                System.out.println("Thứ năm");
+                break;
+            case 5:
+                System.out.println("Thứ sáu");
+                break;
+            case 6:
+                System.out.println("Thứ bảy");
+                break;
+            case 7:
+                System.out.println("Chủ nhật");
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
+
+    
